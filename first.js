5f32884053e302da4b3dd2b293d424e4cb578cb2
aditidata <155631327+aditidata@users.noreply.github.com>
2025-08-13 20:29:30 +0530

    JS practice set1

--- first.js
+++ first.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const product={
+    name:"ball pen",
+    rating:4,
+    offer:5,
+    price: 210,
+
+};
+console.log(product);
+const profile={
+name:"Aditi",
+age:21,
+city:"Bangalore",
+isFollow:true,
+
+}
+console.log(profile);
