e0182d7cc8515c92988e3caead25037b7c44cd07
aditidata <155631327+aditidata@users.noreply.github.com>
2025-08-15 10:04:10 +0530

    JS practice set2

--- first.js
+++ first.js
@@ -1,16 +1,15 @@
-const product={
-    name:"ball pen",
-    rating:4,
-    offer:5,
-    price: 210,
-
-};
-console.log(product);
-const profile={
-name:"Aditi",
-age:21,
-city:"Bangalore",
-isFollow:true,
-
+let score=prompt("enter score");
+if(score>=80){
+    console.log("A");
 }
-console.log(profile);
+else if(score>=70 && score<89){
+    console.log("B");
+}
+else if(score>=60 && score<69){
+    console.log("C");
+}
+else if(score>=50 && score<59){
+    console.log("D");
+}
+else (score>=0 && score<49) 
+    console.log("E");
