eef1071ae6f043dbdfd359abff4ddfb25de5885a
aditidata <155631327+aditidata@users.noreply.github.com>
2025-08-15 12:23:28 +0530

    JS for of loop

--- first.js
+++ first.js
@@ -1,15 +1,19 @@
-let score=prompt("enter score");
-if(score>=80){
-    console.log("A");
+// let score=prompt("enter score");
+// if(score>=80){
+//     console.log("A");
+// }
+// else if(score>=70 && score<89){
+//     console.log("B");
+// }
+// else if(score>=60 && score<69){
+//     console.log("C");
+// }
+// else if(score>=50 && score<59){
+//     console.log("D");
+// }
+// else (score>=0 && score<49) 
+//     console.log("E");
+let str="Aditi";
+for(let i of str){
+    console.log(i);
 }
-else if(score>=70 && score<89){
-    console.log("B");
-}
-else if(score>=60 && score<69){
-    console.log("C");
-}
-else if(score>=50 && score<59){
-    console.log("D");
-}
-else (score>=0 && score<49) 
-    console.log("E");
