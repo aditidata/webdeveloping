f27ad2025e74565cda7265fb0e95e42977ab80af
aditidata <155631327+aditidata@users.noreply.github.com>
2025-08-18 13:44:35 +0530

    Changing modes onclick

--- first.js
+++ first.js
@@ -146,13 +146,26 @@
 //     div.innerText=`new value ${idx}`;
 //     idx++;
 // }
-let newBtn=document.createElement("Button");
-newBtn.innerText="click me!";
-newBtn.style.background="red";
-newBtn.style.color="white";
-document.querySelector("body").prepend(newBtn);
+// let newBtn=document.createElement("Button");
+// newBtn.innerText="click me!";
+// newBtn.style.background="red";
+// newBtn.style.color="white";
+// document.querySelector("body").prepend(newBtn);
 
-//q2
-let para=document.querySelector("p");
-//overwrite class name while adding a new one
-para.classList.add("newClass");
+// //q2
+// let para=document.querySelector("p");
+// //overwrite class name while adding a new one
+// para.classList.add("newClass");
+let modeBtn=document.querySelector('#mode');
+let currMode="light";
+modeBtn.addEventListener("click",()=>{
+    if(currMode==="light"){
+        currMode="dark";
+        document.querySelector("body").style.backgroundColor="black";
+    }
+    else{
+        currMode="light";
+        document.querySelector("body").style.backgroundColor="white";
+        }
+        console.log(currMode);
+})
