389f42596a852576f33b453b8774927b1a8d6e25
aditidata <155631327+aditidata@users.noreply.github.com>
2025-08-18 19:53:06 +0530

    Stone Paper Scissors Game

--- first.js
+++ first.js
@@ -156,16 +156,85 @@
 // let para=document.querySelector("p");
 // //overwrite class name while adding a new one
 // para.classList.add("newClass");
-let modeBtn=document.querySelector('#mode');
-let currMode="light";
-modeBtn.addEventListener("click",()=>{
-    if(currMode==="light"){
-        currMode="dark";
-        document.querySelector("body").style.backgroundColor="black";
+// let modeBtn=document.querySelector('#mode');
+// let currMode="light";
+// modeBtn.addEventListener("click",()=>{
+//     if(currMode==="light"){
+//         currMode="dark";
+//         document.querySelector("body").style.backgroundColor="black";
+//     }
+//     else{
+//         currMode="light";
+//         document.querySelector("body").style.backgroundColor="white";
+//         }
+//         console.log(currMode);
+// })
+let userScore = 0;
+let compScore = 0;
+const choices = document.querySelectorAll(".choice");
+const msg=document.querySelector("#msg");
+const UserscorePara= document.querySelector("#user-score");
+const compScorePara  = document.querySelector("#comp-score");
+const genCompChoice = () => {
+    const options = ["rock","paper","scissors"];
+    const randIdx = Math.floor(Math.random() * 3);  // FIXED
+    return options[randIdx];
+}
+
+const drawGame = () => {
+    console.log("game is draw");
+            msg.innerText="Draw!";
+
+}
+
+const showWinner = (userWin) => {   // FIXED
+    if(userWin){
+        userScore++;
+        UserscorePara.innerText=userScore;
+        console.log("u winn");
+        msg.innerText="u winn";
+        msg.style.backgroundColor="green";
     }
     else{
-        currMode="light";
-        document.querySelector("body").style.backgroundColor="white";
-        }
-        console.log(currMode);
-})
+        compScore++;
+        compScorePara.innerText=compScore;
+        console.log("comp win");
+        msg.innerText="u lose";
+        msg.style.backgroundColor="red";
+
+    }
+}
+
+const playGame = (userChoice) => {
+    console.log("userchoice=", userChoice);
+
+    // comp choice
+    const compChoice = genCompChoice();
+    console.log("comp choice=", compChoice);
+
+    if(userChoice === compChoice){
+        drawGame();
+        return;  // prevent further checking
+    }
+
+    let userWin = true;
+
+    if(userChoice === "rock"){
+        userWin = compChoice === "paper" ? false : true;
+    }
+    else if(userChoice === "paper"){
+        userWin = compChoice === "scissors" ? false : true;
+    }
+    else if(userChoice === "scissors"){
+        userWin = compChoice === "rock" ? false : true;
+    }
+
+    showWinner(userWin); // FIXED
+}
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
