b5bdfaec41b6479e22ac626d0a665e6efa16808c
aditidata <155631327+aditidata@users.noreply.github.com>
2025-09-01 00:29:05 +0530

    modify styles

--- first.js
+++ first.js
@@ -256,14 +256,10 @@
 //         alert(`congrats u reached ${counter}`);
 //     }
 // }
-document.addEventListener("DOMContentLoaded",function(){
-    document.querySelector('#red').onclick=function(){
-        document.body.style.backgroundColor="red";
-    };
-    document.querySelector('#green').onclick=function(){
-        document.body.style.backgroundColor="green";
-    }
-    document.querySelector('#blue').onclick=function(){
-        document.body.style.backgroundColor="blue";
-    }
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('button').forEach(function (button) {
+        button.onclick = function () {
+            document.querySelector('#hello').style.color = button.dataset.color;
+        };
+    });
 });
