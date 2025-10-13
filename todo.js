4b2b2aae4291cf7a3c7a2c433c313a374bd53ca6
aditidata <155631327+aditidata@users.noreply.github.com>
2025-10-13 20:01:22 +0530

    reduce method acc and el

--- todo.js
+++ todo.js
@@ -1,29 +1,35 @@
-let todo=[];
+let nums=[1,2,3,4];
+let finalValue=nums.reduce((accumulator,currentValue)=>{
+    return accumulator+currentValue;
+}
+,0);
+console.log(finalValue);
+// let todo=[];
 
-let req=prompt("enter your request");
-while(true){
-    if(req=="quit"){
-        console.log("bye");
-        break;
-    }
-    if(req=="list"){
-        console.log("**********");
-        for(let i=0;i<todo.length;i++){
-            console.log(i+" : "+todo[i]);
-        }
-        console.log("**********");
-    }
-    else if(req=="add"){
-        let task=prompt("enter task");
-        todo.push(task);
-        console.log(`${task} added to the list`);
-    }
-    else if(req=="delete"){
-        let index=prompt("enter index to delete");  
-        if(index>=0 && index<todo.length){
-        let deleted=todo.splice(index,1);
-        console.log(`deleted ${deleted}`);
-        }
-}
-    req=prompt("enter your request");
-}
+// let req=prompt("enter your request");
+// while(true){
+//     if(req=="quit"){
+//         console.log("bye");
+//         break;
+//     }
+//     if(req=="list"){
+//         console.log("**********");
+//         for(let i=0;i<todo.length;i++){
+//             console.log(i+" : "+todo[i]);
+//         }
+//         console.log("**********");
+//     }
+//     else if(req=="add"){
+//         let task=prompt("enter task");
+//         todo.push(task);
+//         console.log(`${task} added to the list`);
+//     }
+//     else if(req=="delete"){
+//         let index=prompt("enter index to delete");  
+//         if(index>=0 && index<todo.length){
+//         let deleted=todo.splice(index,1);
+//         console.log(`deleted ${deleted}`);
+//         }
+// }
+//     req=prompt("enter your request");
+// }
