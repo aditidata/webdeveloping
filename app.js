f4c2a4e5d641bae8b5deb5a6fee2bc3c575d4ead
aditidata <155631327+aditidata@users.noreply.github.com>
2025-10-14 20:21:18 +0530

    DOM color manipulation

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ let smallImg=document.getElementsByClassName("oldImg");
 for(let i=0;i<smallImg.length;i++){
     console.dir(smallImg[i].src);
 }
+let links=document.querySelectorAll(".box a");
+for(let i=0;i<links.length;i++){
+   links[i].style.color="red";
+}
