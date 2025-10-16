2e58f1a281598cf9181ff6eeab9e7b90482c3f68
aditidata <155631327+aditidata@users.noreply.github.com>
2025-10-16 23:43:51 +0530

    EventListeners

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ let para=document.createElement('p');
 para.innerText="Hello World!";
 document.querySelector('body').appendChild(para);
 para.classList.add('green');
+let btns=document.querySelectorAll('button');
+for(btn of btns){
+    btn.addEventListener("click",sayHello);
+    
+}
+function sayHello(){
+    alert("Hello World!");
+}
