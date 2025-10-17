4f02051485307c44dc306bbe16c7ce3bf9bd698e
aditidata <155631327+aditidata@users.noreply.github.com>
2025-10-17 23:27:18 +0530

    adding math.random to generate values

--- activity.js
+++ activity.js
@@ -3,9 +3,5 @@ btn.addEventListener("click",function(){
     console.log("Button Clicked!");
 });
 function getRandomColor(){
-    let red=Math.floor(Math.random()*255);
-    let green=Math.floor(Math.random()*255);
-    let blue=Math.floor(Math.random()*255);
-    let color= `(${red},${green},${blue})`;
-    return color;
+    
 }
