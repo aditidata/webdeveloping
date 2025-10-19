398cb0030cf84aec852e776f394a36d4153baabb
aditidata <155631327+aditidata@users.noreply.github.com>
2025-10-19 23:24:07 +0530

    task delegation

--- todo.js
+++ todo.js
@@ -2,6 +2,19 @@ let btn=document.querySelector('button');
 let ul=document.querySelector('ul');
 let input=document.querySelector('input');
 btn.addEventListener("click",function(){
-    console.log(input.value);
+    let item=document.createElement('li');
+    item.innerText=input.value;
+    let delbtn=document.createElement('button');
+    delbtn.innerText="delete";
+    delbtn.classList.add("delete");
+    item.appendChild(delbtn);
+    ul.appendChild(item);
     input.value=""
 });
+let deleteButtons=document.querySelectorAll('.delete');
+for(let delbtn of deleteButtons){
+    delbtn.addEventListener("click",function(event){
+        let parent=delbtn.parentElement;
+        console.log(parent);
+        parent.remove();
+    })};
