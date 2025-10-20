735bf1810e089a45f8ea1a541ed20101cf5c148d
aditidata <155631327+aditidata@users.noreply.github.com>
2025-10-20 16:24:50 +0530

    button flash

--- simon.js
+++ simon.js
@@ -1,11 +1,29 @@
 let gameseq=[];
 let userseq=[];
-
+let btns=["red","blue","green","yellow"];
 let started=false;
 let level=0;
-
+let h2=document.querySelector('h2');
 document.addEventListener("keypress",function(){
-    if(started===false){
+    if(started==false){
         console.log("game started");
         started=true;
+        levelup();
     }});
+
+    function btnFlash(btn){
+        btn.classList.add("flash");
+        setTimeout(function(){
+            btn.classList.remove("flash");
+        },500);
+    }
+    function levelup(){
+        level++;
+        h2.innerText="level "+level;
+
+        let randomIndex=Math.floor(Math.random()*3);
+        let randomColor=btns[randomIndex];
+        let randomBtn=document.querySelector(`.${randomColor}`);
+        btnFlash(randomBtn);
+        btnFlash();
+    }
