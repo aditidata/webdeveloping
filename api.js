4f8481137457e8b9608028f7c39791a12b7d47b7
aditidata <155631327+aditidata@users.noreply.github.com>
2025-11-04 20:00:37 +0530

    await to fetch api

--- api.js
+++ api.js
@@ -1,11 +1,17 @@
 let url="https://catfact.ninja/fact";
-fetch(url)
-    .then((response)=>{
-        console.log(response);
-        response.json().then((data)=>{
-            console.log(data);
-        });
-    })
-    .catch((error)=>{
-        console.log(error);
-    });
+// fetch(url)
+//     .then((response)=>{
+//         console.log(response);
+//         response.json().then((data)=>{
+//             console.log(data);
+//         });
+//     })
+//     .catch((error)=>{
+//         console.log(error);
+//     });
+async function getFact(){
+    let result= await fetch(url);
+    let data= await result.json();
+
+    console.log(data.fact);
+}
