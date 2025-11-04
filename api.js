a24da299aa57abf51e4e711ea20897241dcc1be5
aditidata <155631327+aditidata@users.noreply.github.com>
2025-11-04 20:12:26 +0530

    axios

--- api.js
+++ api.js
@@ -1,4 +1,15 @@
 let url="https://catfact.ninja/fact";
+
+async function getFact(){
+    try{
+        let response= await axios.get(url);
+        console.log(response);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
 // fetch(url)
 //     .then((response)=>{
 //         console.log(response);
@@ -9,9 +20,11 @@ let url="https://catfact.ninja/fact";
 //     .catch((error)=>{
 //         console.log(error);
 //     });
-async function getFact(){
-    let result= await fetch(url);
-    let data= await result.json();
 
-    console.log(data.fact);
-}
+
+// async function getFact(){
+//     let result= await fetch(url);
+//     let data= await result.json();
+
+//     console.log(data.fact);
+// }
