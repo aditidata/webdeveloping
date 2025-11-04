4df4ecd87ffefd1e863b49e6a73697f9d0d89a5a
aditidata <155631327+aditidata@users.noreply.github.com>
2025-11-04 20:24:01 +0530

    showing facts on btn click

--- api.js
+++ api.js
@@ -1,12 +1,19 @@
+let btn=document.querySelector('button');
+btn.addEventListener("click", async()=>{
+    let fact= await getFact();
+    let p=document.querySelector('p');
+    p.innerText=fact;
+});
 let url="https://catfact.ninja/fact";
 
 async function getFact(){
     try{
         let response= await axios.get(url);
-        console.log(response);
+        return response.data.fact;
     }
     catch(error){
         console.log(error);
+        return"no fact found";
     }
 }
 
