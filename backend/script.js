189a2f2a6c91baeec840972ecf766dc16ab326ec
aditidata <155631327+aditidata@users.noreply.github.com>
2025-12-01 21:15:52 +0530

    arguments in node

--- backend/script.js
+++ backend/script.js
@@ -1,4 +1,8 @@
-let n=5;
-for(let i=1;i<=n;i++){
-    console.log("hell0" +i);
+// let n=5;
+// for(let i=1;i<=n;i++){
+//     console.log("hell0" +i);
+// }
+let args=process.argv;
+for(let i=2;i<args.length;i++){
+    console.log("hello"+args[i]);
 }
