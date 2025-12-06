2ac568458206fe47083877aa11babea483db8417
aditidata <155631327+aditidata@users.noreply.github.com>
2025-12-06 22:57:37 +0530

    export and require

---
+++ backend/math.js
@@ -0,0 +1,4 @@
+const pi=3.14;
+const e=2.71;
+
+module.exports=123;
--- backend/script.js
+++ backend/script.js
@@ -6,3 +6,5 @@ let args=process.argv;
 for(let i=2;i<args.length;i++){
     console.log("hello"+args[i]);
 }
+const someValue=require('./math.js');
+console.log(someValue);
