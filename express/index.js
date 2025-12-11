e5bfeaa127b173b7f632f5dce7837d4b6d7e39a1
aditidata <155631327+aditidata@users.noreply.github.com>
2025-12-11 14:29:07 +0530

    sending response on the localserver

--- express/index.js
+++ express/index.js
@@ -4,3 +4,9 @@ let port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+app.use((req,res)=>{
+    console.log('Request received');
+    res.send({
+        message: 'Hello from Express server!'
+    });
+});
